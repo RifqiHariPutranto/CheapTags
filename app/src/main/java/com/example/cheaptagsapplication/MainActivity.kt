5edb2885b26208450011ab2bdf4fdaf9122265e2
RifqiHariPutranto <rifqiharip@gmail.com>
2023-12-10 19:52:45 +0700

    12/10/23 Updated Settings Add Dark Mode

--- app/src/main/java/com/example/cheaptagsapplication/MainActivity.kt
+++ app/src/main/java/com/example/cheaptagsapplication/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
 //        setSupportActionBar(toolbar)
 
+
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
         val headerView = navigationView.getHeaderView(0)
         navigationView.setNavigationItemSelectedListener(this)
