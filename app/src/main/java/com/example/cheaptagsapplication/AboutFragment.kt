bb517cddadf9f8125909ae7af1fa06ea7aa6b00b
RifqiHariPutranto <rifqiharip@gmail.com>
2023-12-13 12:23:32 +0700

    12/13/23 Updated About US Page

--- app/src/main/java/com/example/cheaptagsapplication/AboutFragment.kt
+++ app/src/main/java/com/example/cheaptagsapplication/AboutFragment.kt
@@ -1,9 +1,13 @@
 package com.example.cheaptagsapplication
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 
@@ -19,7 +23,22 @@ class AboutFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val v = inflater.inflate(R.layout.fragment_about, container, false)
+        val git = v.findViewById<ImageButton>(R.id.git_btn)
+        val git_txt = v.findViewById<TextView>(R.id.git_text)
+
+        git.setOnClickListener {
+            val openURL= Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data= Uri.parse("https://github.com/RifqiHariPutranto/CheapTags")
+            startActivity(openURL)
+        }
+        git_txt.setOnClickListener {
+            val openURL= Intent(android.content.Intent.ACTION_VIEW)
+            openURL.data= Uri.parse("https://github.com/RifqiHariPutranto/CheapTags")
+            startActivity(openURL)
+        }
+
+        return v
     }
 
 
